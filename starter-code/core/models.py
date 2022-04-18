94bcfbdf96cbc135b57080a11f79070589bdbaf4
Simone Dalla <simodalla@gmail.com>
2022-04-18 11:30:36 +0200

    ..

--- starter-code/core/models.py
+++ starter-code/core/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    class Subjects(models.IntegerChoices):
+        WEB_DEVELOPMENT = 1
+        SYSTEMS_PROGRAMMING = 2
+        DATA_SCIENCES = 3
+
+    subject = models.PositiveSmallIntegerField(choices=Subjects.choices)
+    date_of_birth = models.DateField()
--- starter-code/core/views.py
+++ starter-code/core/views.py
@@ -1,6 +1,24 @@
+from crispy_forms.utils import render_crispy_form
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.template.context_processors import csrf
+
+from .forms import UniversityForm
+
 
 # Create your views here.
 def index(request):
-    context = {}
-    return render(request, 'index.html', context)
+    if request.method == "GET":
+        context = {"form": UniversityForm()}
+        return render(request, "index.html", context)
+
+    form = UniversityForm(request.POST)
+    if form.is_valid():
+        user = form.save()
+        return HttpResponse("Hi")
+
+    # https://django-crispy-forms.readthedocs.io/en/latest/crispy_tag_forms.html#ajax-validation-recipe
+    ctx = {}
+    ctx.update(csrf(request))
+    form_html = render_crispy_form(form, context=ctx)
+    return HttpResponse(form_html)
