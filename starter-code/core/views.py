f9652955086eab49d55cf3d5bc01d72a263cc48b
Simone Dalla <simodalla@gmail.com>
2022-07-22 19:34:22 +0200

    Revert "Revert "aggiunta file per gestione codice e per git""
    This reverts commit e79ffc2a7738e1a8400b56fb89e6ffc3c815eab3.

--- starter-code/core/views.py
+++ starter-code/core/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 
+from .forms import UniversityForm
+
+
 # Create your views here.
 def index(request):
-    context = {}
-    return render(request, 'index.html', context)
+    context = {"form": UniversityForm()}
+    return render(request, "index.html", context)
